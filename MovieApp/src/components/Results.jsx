2e24eeb43d12243191a93391fcc7152fd5dea98d
Bob <rbsavary88@gmail.com>
2023-05-18 19:03:47 -0400

    Add card to Results

--- MovieApp/src/components/Results.jsx
+++ MovieApp/src/components/Results.jsx
@@ -4,7 +4,12 @@ import {Image, Text, StyleSheet, View} from 'react-native'
 const Results = ({movie}) => {
   return (
   <View style={styles.card}>
-    
+    <Image style={styles.image} src={movie.Poster} />
+    <View style={styles.content}>
+      <Text>{movie.Title}</Text>
+      <Text>{movie.Rated} {movie.Year}</Text>
+      <Text>{movie.Plot}</Text>
+    </View>
   </View>)
 }
 
