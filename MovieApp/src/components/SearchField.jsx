3fbca1cbf863df324c637d78c1a5b6261d300c90
Bob <rbsavary88@gmail.com>
2023-05-18 14:58:00 -0400

    React native app initial commit

---
+++ MovieApp/src/components/SearchField.jsx
@@ -0,0 +1,12 @@
+import {StyleSheet, Text, View} from 'react-native';
+
+
+const SearchField = () => {
+  return (
+    <View>
+      <Text>Search Field</Text>
+    </View>
+  )
+}
+
+export default SearchField
---
+++ MovieApp/src/screens/LandingPage.jsx
@@ -0,0 +1,12 @@
+import {Text, View} from "react-native"
+import Banner from "../components/Banner"
+const LandingScreen = () => {
+  return (
+  <View style={{flex: 1}}>
+    <Banner />
+  </View>
+  )
+}
+
+
+export default LandingScreen
