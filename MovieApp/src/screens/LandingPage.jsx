4a22f549b4cb9913ba919c246f52e1e1784c1d25
Bob <rbsavary88@gmail.com>
2023-05-18 16:51:24 -0400

    Add SearchField

--- MovieApp/src/screens/LandingPage.jsx
+++ MovieApp/src/screens/LandingPage.jsx
@@ -1,9 +1,11 @@
 import {Text, View} from "react-native"
 import Banner from "../components/Banner"
+import SearchField from "../components/SearchField"
 const LandingScreen = () => {
   return (
   <View style={{flex: 1}}>
     <Banner />
+    <SearchField />
   </View>
   )
 }
